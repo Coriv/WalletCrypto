ce843470c258d47734b8408c410898a53fe1d967
Sebastian Boron <sebastian.boron8@gmail.com>
2023-06-25 11:50:14 +0200

    initial service

---
+++ src/main/java/com/microservices/walletcrypto/WalletCryptoApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.walletcrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletCryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletCryptoApplication.class, args);
+    }
+
+}
